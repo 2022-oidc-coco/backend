581e59c44c6309bbb5a8be503c40524f07f5a048
skm0626 <skm0626@kookmin.ac.kr>
2022-07-27 23:01:15 +0900

    [Fix] DB에 데이터 저장이 안되는 문제 해결

--- extractlocation/cocokm/views.py
+++ extractlocation/cocokm/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from cocokm.data.collecting_data import collect_data
 from cocokm.data.data_processing import data_processing_
 from cocokm.forms import *
+from decimal import Decimal
+from datetime import datetime
 
 irum = ""
 
@@ -28,16 +30,26 @@ def insert(request):
         irum = request.POST["name"]
         dataset = collect_data(irum,'viewCount')
         records = data_processing_(irum,dataset)
-        # print(records)
+        print(records)
         for i in range(len(records)):
-            form = Form(records[i])
-            print(form)
-            if form.is_valid():
-                form.save()
+            # form = Form(records[i])
+            form = locationInfo(
+            place_name = records[i]['place_name'][0],
+            x=Decimal(records[i]['x'][0]),
+            y =Decimal(records[i]['y'][0]),
+            place_url =records[i]['place_url'][0],
+            category =records[i]['category'][0],
+            video_id=records[i]['video_id'][0],
+            publishTime=(records[i]['publishTime']),
+            viewCount=records[i]['viewCount']
+            )
+            # print(form)
+            # if form.is_valid():
+            form.save()
         locinfo = locationInfo.objects.all()
-        print(locinfo)
-        print("---------------form----------------")
-        print(form)
+        print("DB",locinfo)
+        # print("---------------form----------------")
+        # print(form)
         return render(request, "list.html", {"key":irum, 'locationInfo':locinfo})
     else:
         print("요청실패")
